6490f450149743ee0b524f53f563a37a6caf2034
dpvpp <dpvpp22@yahoo.com>
2021-08-30 15:25:35 -0400

    added humbaba cipher jar to classpath and project files

--- src/main/java/com/example/demo/HumbabaApiApplication.java
+++ src/main/java/com/example/demo/HumbabaApiApplication.java
@@ -3,11 +3,20 @@ package com.example.demo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import humbaba.cipher.CaesarCipher;
+import humbaba.cipher.HumbabaCipher;
+
 @SpringBootApplication
 public class HumbabaApiApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(HumbabaApiApplication.class, args);
+		HumbabaCipher cipher = new CaesarCipher();
+		
+		char[] cipherText = cipher.encrypt("Secret message");
+		System.out.println(cipherText);
+		char[] plainText = cipher.decrypt(cipherText);
+		System.out.println(plainText);
 	}
 
 }
